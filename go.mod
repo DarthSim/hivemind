d6d7c7a497b2a2f1859a82c23f7e45330e659fec
Anbraten <anton@ju60.de>
2023-10-20 17:13:07 +0200

    update to urfave2

--- go.mod
+++ go.mod
@@ -5,13 +5,13 @@ go 1.21
 require (
 	github.com/DarthSim/godotenv v1.3.1
 	github.com/pkg/term v1.2.0-beta.2
-	github.com/urfave/cli v1.22.5
+	github.com/urfave/cli/v2 v2.25.7
 )
 
 require (
-	github.com/cpuguy83/go-md2man/v2 v2.0.0-20190314233015-f79a8a8ca69d // indirect
-	github.com/joho/godotenv v1.4.0 // indirect
-	github.com/russross/blackfriday/v2 v2.0.1 // indirect
-	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
-	golang.org/x/sys v0.0.0-20211124211545-fe61309f8881 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.3 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
+	golang.org/x/sys v0.13.0 // indirect
 )
--- main.go
+++ main.go
@@ -4,12 +4,12 @@ import (
 	"os"
 	"path/filepath"
 
-	"github.com/urfave/cli"
+	"github.com/urfave/cli/v2"
 
 	_ "github.com/DarthSim/godotenv/autoload"
 )
 
-const version = "1.1.0"
+const version = "1.2.0"
 
 func main() {
 	var (
@@ -17,57 +17,101 @@ func main() {
 		err  error
 	)
 
-	app := cli.NewApp()
-
-	app.Name = "Hivemind"
-	app.HelpName = "hivemind"
-	app.Usage = "The mind to rule processes of your development environment"
-	app.Description = "Hivemind is a process manager for Procfile-based applications"
-	app.Author = "Sergey \"DarthSim\" Alexandrovich"
-	app.Email = "darthsim@gmail.com"
-	app.Version = version
-	app.ArgsUsage = "[procfile] (Use '-' to read from stdin, Procfile path can be also set with $HIVEMIND_PROCFILE)"
-	app.HideHelp = true
-
-	app.Flags = []cli.Flag{
-		cli.StringFlag{Name: "title, w", EnvVar: "HIVEMIND_TITLE", Usage: "Specify a title of the application", Destination: &conf.Title},
-		cli.StringFlag{Name: "processes, l", EnvVar: "HIVEMIND_PROCESSES", Usage: "Specify process names to launch. Divide names with comma", Destination: &conf.ProcNames},
-		cli.IntFlag{Name: "port, p", EnvVar: "HIVEMIND_PORT,PORT", Usage: "specify a port to use as the base", Value: 5000, Destination: &conf.PortBase},
-		cli.IntFlag{Name: "port-step, P", EnvVar: "HIVEMIND_PORT_STEP", Usage: "specify a step to increase port number", Value: 100, Destination: &conf.PortStep},
-		cli.StringFlag{Name: "root, d", EnvVar: "HIVEMIND_ROOT", Usage: "specify a working directory of application. Default: directory containing the Procfile", Destination: &conf.Root},
-		cli.IntFlag{Name: "timeout, t", EnvVar: "HIVEMIND_TIMEOUT", Usage: "specify the amount of time (in seconds) processes have to shut down gracefully before being brutally killed", Value: 5, Destination: &conf.Timeout},
-		cli.BoolFlag{Name: "no-prefix", EnvVar: "HIVEMIND_NO_PREFIX", Usage: "process names will not be printed if the flag is specified", Destination: &conf.NoPrefix},
-		cli.BoolFlag{Name: "print-timestamps, T", EnvVar: "HIVEMIND_PRINT_TIMESTAMPS", Usage: "timestamps will be printed if the flag is specified", Destination: &conf.PrintTimestamps},
-	}
-
-	app.Action = func(c *cli.Context) error {
-		switch c.NArg() {
-		case 0:
-			if path := os.Getenv("HIVEMIND_PROCFILE"); len(path) > 0 {
-				conf.Procfile = path
-			} else {
-				conf.Procfile = "./Procfile"
+	app := &cli.App{
+		Name:     "Hivemind",
+		HelpName: "hivemind",
+		Usage:    "The mind to rule processes of your development environment",
+		Authors: []*cli.Author{
+			{
+				Name:  "Sergey \"DarthSim\" Alexandrovich",
+				Email: "darthsim@gmail.com",
+			},
+		},
+		Version:   version,
+		ArgsUsage: "[procfile] (Use '-' to read from stdin, Procfile path can be also set with $HIVEMIND_PROCFILE)",
+		HideHelp:  true,
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:        "title, w",
+				EnvVars:     []string{"HIVEMIND_TITLE"},
+				Usage:       "Specify a title of the application",
+				Destination: &conf.Title,
+			},
+			&cli.StringFlag{
+				Name:        "processes, l",
+				EnvVars:     []string{"HIVEMIND_PROCESSES"},
+				Usage:       "Specify process names to launch. Divide names with comma",
+				Destination: &conf.ProcNames,
+			},
+			&cli.IntFlag{
+				Name:        "port, p",
+				EnvVars:     []string{"HIVEMIND_PORT,PORT"},
+				Usage:       "specify a port to use as the base",
+				Value:       5000,
+				Destination: &conf.PortBase,
+			},
+			&cli.IntFlag{
+				Name:        "port-step, P",
+				EnvVars:     []string{"HIVEMIND_PORT_STEP"},
+				Usage:       "specify a step to increase port number",
+				Value:       100,
+				Destination: &conf.PortStep,
+			},
+			&cli.StringFlag{
+				Name:        "root, d",
+				EnvVars:     []string{"HIVEMIND_ROOT"},
+				Usage:       "specify a working directory of application. Default: directory containing the Procfile",
+				Destination: &conf.Root,
+			},
+			&cli.IntFlag{
+				Name:        "timeout, t",
+				EnvVars:     []string{"HIVEMIND_TIMEOUT"},
+				Usage:       "specify the amount of time (in seconds) processes have to shut down gracefully before being brutally killed",
+				Value:       5,
+				Destination: &conf.Timeout,
+			},
+			&cli.BoolFlag{
+				Name:        "no-prefix",
+				EnvVars:     []string{"HIVEMIND_NO_PREFIX"},
+				Usage:       "process names will not be printed if the flag is specified",
+				Destination: &conf.NoPrefix,
+			},
+			&cli.BoolFlag{
+				Name:        "print-timestamps, T",
+				EnvVars:     []string{"HIVEMIND_PRINT_TIMESTAMPS"},
+				Usage:       "timestamps will be printed if the flag is specified",
+				Destination: &conf.PrintTimestamps,
+			},
+		},
+		Action: func(c *cli.Context) error {
+			switch c.NArg() {
+			case 0:
+				if path := os.Getenv("HIVEMIND_PROCFILE"); len(path) > 0 {
+					conf.Procfile = path
+				} else {
+					conf.Procfile = "./Procfile"
+				}
+			case 1:
+				conf.Procfile = c.Args().First()
+			default:
+				fatal("Specify a single procfile")
 			}
-		case 1:
-			conf.Procfile = c.Args().First()
-		default:
-			fatal("Specify a single procfile")
-		}
 
-		if conf.Timeout < 1 {
-			fatal("Timeout should be greater than 0")
-		}
+			if conf.Timeout < 1 {
+				fatal("Timeout should be greater than 0")
+			}
 
-		if len(conf.Root) == 0 {
-			conf.Root = filepath.Dir(conf.Procfile)
-		}
+			if len(conf.Root) == 0 {
+				conf.Root = filepath.Dir(conf.Procfile)
+			}
 
-		conf.Root, err = filepath.Abs(conf.Root)
-		fatalOnErr(err)
+			conf.Root, err = filepath.Abs(conf.Root)
+			fatalOnErr(err)
 
-		newHivemind(conf).Run()
+			newHivemind(conf).Run()
 
-		return nil
+			return nil
+		},
 	}
 
 	app.Run(os.Args)
